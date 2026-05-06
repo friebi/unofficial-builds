5f123e49e0ff303b654a99020eda39bf76bf3607
friebi <friebi@gmx.de>
2026-05-06 22:08:35 +0200

    it compiles!

--- recipes/armv6l/run.sh
+++ recipes/armv6l/run.sh
@@ -29,8 +29,8 @@ cd "node-${fullversion}"
 export CCACHE_BASEDIR="$PWD"
 export CC_host="ccache clang-19"
 export CXX_host="ccache clang++-19"
-export CC="ccache clang-19 --target=arm-linux-gnueabihf -march=armv6zk -mfpu=vfp -mfloat-abi=hard -U__ILP32__"
-export CXX="ccache clang++-19 --target=arm-linux-gnueabihf -march=armv6zk -mfpu=vfp -mfloat-abi=hard -U__ILP32__"
+export CC="ccache clang-19 --target=arm-linux-gnueabihf -march=armv6zk -mfpu=vfp -mfloat-abi=hard -U__ILP32__ -Xclang -target-feature -Xclang +db"
+export CXX="ccache clang++-19 --target=arm-linux-gnueabihf -march=armv6zk -mfpu=vfp -mfloat-abi=hard -U__ILP32__ -Xclang -target-feature -Xclang +db"
 
 make -j$(getconf _NPROCESSORS_ONLN) binary V= \
   DESTCPU="arm" \
